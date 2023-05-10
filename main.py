06adebc03db8c6ab0837cab5cbcc80a417e9950f
Apoool <pauljohnquiros14@gmail.com>
2023-05-10 16:55:31 +0800

    fixed code structures

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ import os
 import shutil
 import cv2
 
-button = 12
+
+button = 18
 TRIG = 16
 ECHO = 15
 path = "/home/caneaid/Desktop/Main/results"
 path_file = "/home/caneaid/Desktop/Main/results/image.txt"
 path_audio = "/home/caneaid/Desktop/Main/audio/"
+model = "model_1" #default
+path_model = "/home/caneaid/Desktop/Main/models_dir/selected_model.txt"
 
 pygame.mixer.init()
 print(cv2.__version__)
+
+def readModel():
+	with open(path_model, "r") as f:
+		model = f.readlines()	
+		print(model)
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
@@ -22,6 +30,7 @@ def setup():
 	GPIO.setup(TRIG,GPIO.OUT)
 	GPIO.setup(ECHO,GPIO.IN)
 	GPIO.output(TRIG, False)
+	readModel()
 
 def loop():
 	pygame.mixer.music.load(path_audio+ "good_day.mp3")
@@ -42,28 +51,21 @@ def loop():
 		pulse_duration = pulse_end - pulse_start
 		distance = pulse_duration * 17150
 		distance = round(distance+1.15)
-		#this will be used for automatic image capture and processing depends on consultation
-		#will also be used for little navigation feature together with other 2 sensors
-		#change to 150 cm (150cm = 1.5m) according to papers
-		#20 cm for testing
-		if distance <= 150:
-			print(distance)
-			#autostart testing
-			pygame.mixer.music.load(path_audio + "obstacle_close.mp3")
-			pygame.mixer.music.play()
-			while pygame.mixer.music.get_busy() == True:
-				continue
-			print("too close")
+
 		#Button for image capture and processing
 		if  button_state == False:
 			print('Button Pressed...')
 			print(distance)
+			pygame.mixer.music.load(path_audio+"btn_prs.mp3")
+			pygame.mixer.music.play()
+			while pygame.mixer.music.get_busy() == True:
+				continue
 			
 			#image capture
 			subprocess.call(["raspistill", "-t", "500", "-o", "/home/caneaid/Desktop/Main/image.jpg"])
 			#image object recognition
 			subprocess.call(["python", "/home/caneaid/Desktop/Main/core/image_detection.py", 
-			"--modeldir=/home/caneaid/Desktop/Main/models_dir/model_1", "--threshold=.7", "--save_results", "--image=/home/caneaid/Desktop/Main/image.jpg"])
+			f"--modeldir=/home/caneaid/Desktop/Main/models_dir/{model}", "--threshold=.6", "--save_results", "--image=/home/caneaid/Desktop/Main/image.jpg"])
 			
 			#If no objects captured text to speech wont be executed
 			print(os.stat(path_file).st_size)
@@ -112,6 +114,83 @@ def loop():
 				print('?')
 				time.sleep(0.2)
 
+		#this will be used for automatic image capture and processing depends on consultation
+		#will also be used for little navigation feature together with other 2 sensors
+		#change to 150 cm (150cm = 1.5m) according to papers
+		#20 cm for testing
+		if distance <= 150:
+			print(distance)
+			#autostart testing
+			pygame.mixer.music.load(path_audio + "obstacle_close.mp3")
+			pygame.mixer.music.play()
+			while pygame.mixer.music.get_busy() == True:
+				continue
+			print("too close")
+			
+					#Button for image capture and processing
+			if  button_state == False:
+				print('Button Pressed...')
+				print(distance)
+				pygame.mixer.music.load(path_audio+"btn_prs.mp3")
+				pygame.mixer.music.play()
+				while pygame.mixer.music.get_busy() == True:
+					continue
+			
+				#image capture
+				subprocess.call(["raspistill", "-t", "500", "-o", "/home/caneaid/Desktop/Main/image.jpg"])
+				#image object recognition
+				subprocess.call(["python", "/home/caneaid/Desktop/Main/core/image_detection.py", 
+				f"--modeldir=/home/caneaid/Desktop/Main/models_dir/{model}", "--threshold=.6", "--save_results", "--image=/home/caneaid/Desktop/Main/image.jpg"])
+			
+				#If no objects captured text to speech wont be executed
+				print(os.stat(path_file).st_size)
+				if os.stat(path_file).st_size != 0:
+					with open("/home/caneaid/Desktop/Main/results/image.txt", "r") as f:
+						content = f.readlines()
+
+					# Create a list to store the words
+					words = []
+
+					# Iterate over each line in the file
+					for line in content:
+						# Remove any whitespace or newline characters
+						word = line.strip()
+						# Add the word to the list
+						words.append(word)
+					print(words)
+					print(len(words))
+				
+					for x in words:
+						print(x)
+						pygame.mixer.music.load(path_audio+ x + ".mp3")
+						#pygame.mixer.music.load("ako.mp3")
+						pygame.mixer.music.play()
+						while pygame.mixer.music.get_busy() == True:
+							continue
+						
+					#to reset the results path by deleting it
+					if os.path.exists(path):
+						# delete the directory
+						shutil.rmtree(path)
+						print("Folder deleted successfully")
+					else:
+						print("Folder does not exist")
+				
+				
+				else:
+					pygame.mixer.music.load(path_audio+ "no_obstacle.mp3")
+					pygame.mixer.music.play()
+					while pygame.mixer.music.get_busy() == True:
+						continue
+					print("EMPTY")
+				
+				#If button is still pressed this loop will run
+				while GPIO.input(button) == False:
+					print('?')
+					time.sleep(0.2)
+
+
+			
 def endprogram():
 	GPIO.cleanup()
 
--- main_gui.py
+++ main_gui.py
@@ -11,7 +11,7 @@ class App:
                 self.master.geometry("{0}x{1}+0+0".format(self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
                 self.master.title("CaneAid")
                 print("height:" + str(self.master.winfo_screenheight()) + "width:" + str(self.master.winfo_screenwidth()))
-                
+                self.master.configure(bg="#EFE7BC")
                 self.model_list = []
                 self.object_list = []
                 self.model = 'model_1'
@@ -31,17 +31,17 @@ class App:
                 
                 # Image logo of cane aid
                 self.image = tk.PhotoImage(file="/home/caneaid/Desktop/Main/image/CaneAid.png")
-                self.label = tk.Label(self.master, image=self.image, height=150)
+                self.label = tk.Label(self.master, image=self.image, height=150, bg="#EFE7BC")
                 self.label.pack()
                 
                 # Frame for menu
-                self.menu_frame = tk.Frame(self.master, width=1200, height=100)
+                self.menu_frame = tk.Frame(self.master, width=1200, height=100, bg="#EFE7BC")
                 self.menu_frame.pack(padx=20, fill="x")        
                 
 		# Create a dropdown menu list
                 self.file_list = tk.StringVar(self.master)
                 
-                self.dropdown_menu = tk.Menubutton(self.menu_frame, text="Select model", relief="raised")
+                self.dropdown_menu = tk.Menubutton(self.menu_frame, text="Select model", relief="raised", bg="#74BDCB")
                 self.dropdown_menu.menu = tk.Menu(self.dropdown_menu, tearoff=0)
                 self.dropdown_menu["menu"] = self.dropdown_menu.menu
                 self.dropdown_menu.pack(side=tk.TOP, padx=(270, 0), pady=(10,5))
@@ -50,34 +50,34 @@ class App:
                         
                 
                 # Frame for general info
-                self.general_info_frame = tk.Frame(self.master, width=1200, height=100, bd=5, relief=tk.RIDGE)
+                self.general_info_frame = tk.Frame(self.master, width=1200, height=100, bd=5, relief=tk.RIDGE, bg="#EFE7BC")
                 self.general_info_frame.pack(padx=20, fill="x")
                 
                 # Label_frame
-                self.label_frame = tk.Frame(self.general_info_frame)
+                self.label_frame = tk.Frame(self.general_info_frame, bg="#EFE7BC")
                 self.label_frame.pack(side=tk.TOP, fill="x")
                 
                 #Label
-                self.device_information_label = tk.Label(self.label_frame, text="Device Information:", font=("Arial", 20))
+                self.device_information_label = tk.Label(self.label_frame, text="Device Information:", font=("Arial", 20), bg="#EFE7BC")
                 self.device_information_label.pack(side=tk.LEFT, padx=(0, 400), pady=(5,10))
-                self.model_contents_label = tk.Label(self.label_frame, text="Model Contents:", font=("Arial", 20))
+                self.model_contents_label = tk.Label(self.label_frame, text="Model Contents:", font=("Arial", 20), bg="#EFE7BC")
                 self.model_contents_label.pack(side=tk.RIGHT, padx=(0, 390), pady=(5))
                 
                 
                 # Device Information
-                self.device_info_frame = tk.Frame(self.general_info_frame,height=100)
+                self.device_info_frame = tk.Frame(self.general_info_frame,height=100, bg="#EFE7BC")
                 self.device_info_frame.pack(side=tk.LEFT, fill="both")
-                self.name_label = tk.Label(self.device_info_frame, anchor="w", text=f"Device Name: {self.temp_device_name}", font=("Arial", 12))
+                self.name_label = tk.Label(self.device_info_frame, anchor="w", text=f"Device Name: {self.temp_device_name}", font=("Arial", 12), bg="#EFE7BC")
                 self.name_label.pack(side=tk.TOP, pady=(25,10), fill="both")
-                self.model_name_label = tk.Label(self.device_info_frame, anchor="w", text=f"Model Used: {self.model}", font=("Arial", 12))
+                self.model_name_label = tk.Label(self.device_info_frame, anchor="w", text=f"Model Used: {self.model}", font=("Arial", 12), bg="#EFE7BC")
                 self.model_name_label.pack(side=tk.TOP, pady=12, fill="both")
-                self.open_cv_label = tk.Label(self.device_info_frame, anchor="w", text=f"Open CV Version: {self.temp_opencv_ver}", font=("Arial", 12))
+                self.open_cv_label = tk.Label(self.device_info_frame, anchor="w", text=f"Open CV Version: {self.temp_opencv_ver}", font=("Arial", 12), bg="#EFE7BC")
                 self.open_cv_label.pack(side=tk.TOP, pady=12, fill="both")
-                self.raspberry_os_label = tk.Label(self.device_info_frame, anchor="w", text=f"Raspberry OS: {self.temp_raspberry_os}", font=("Arial", 12))
+                self.raspberry_os_label = tk.Label(self.device_info_frame, anchor="w", text=f"Raspberry OS: {self.temp_raspberry_os}", font=("Arial", 12), bg="#EFE7BC")
                 self.raspberry_os_label.pack(side=tk.TOP, pady=12, fill="both")
-                self.update_label = tk.Label(self.device_info_frame, anchor="w", text=f"Updated On: {self.temp_update}", font=("Arial", 12))
+                self.update_label = tk.Label(self.device_info_frame, anchor="w", text=f"Updated On: {self.temp_update}", font=("Arial", 12), bg="#EFE7BC")
                 self.update_label.pack(side=tk.TOP, pady=12, fill="both")
-                self.software_application_label = tk.Label(self.device_info_frame, anchor="w", text=f"Software Version: {self.temp_software_version}", font=("Arial", 12))
+                self.software_application_label = tk.Label(self.device_info_frame, anchor="w", text=f"Software Version: {self.temp_software_version}", font=("Arial", 12), bg="#EFE7BC")
                 self.software_application_label.pack(side=tk.TOP, pady=12, fill="both")
                 
                 # Create a listbox to display the counter
@@ -85,16 +85,15 @@ class App:
                 self.object_listbox.pack(side=tk.RIGHT, padx=(0, 280), pady=(5))
         
                 # Frame for buttons
-                self.button_frame = tk.Frame(self.master)
+                self.button_frame = tk.Frame(self.master, bg="#EFE7BC")
                 self.button_frame.pack(pady=10)        
         
                 # Create a button that starts the program
-                self.start_button = tk.Button(self.button_frame, text="Save", font=("Arial", 30), command="")
-                self.start_button.pack(side=tk.LEFT, padx=10)
-        
+                self.save_button = tk.Button(self.button_frame, text="Save & Restart", font=("Arial", 24), state="disabled", command=self.restart_system, bg="#74BDCB")
+                self.save_button.pack(side=tk.LEFT, padx=10)
                 # Create a button that stops the program
-                self.stop_button = tk.Button(self.button_frame, text="Restart", font=("Arial", 30), command="")
-                self.stop_button.pack(side=tk.LEFT, padx=10)
+                #self.stop_button = tk.Button(self.button_frame, text="Restart", font=("Arial", 30), command="")
+                #self.stop_button.pack(side=tk.LEFT, padx=10)
                 
                 
                 self.list_model_entity()
@@ -126,7 +125,11 @@ class App:
                 with open(selected_model, 'w') as file:
                         file.writelines(self.model)
                         print(self.model)
-                        
+                self.save_button.config(state="normal")
+                
+        def restart_system(self):
+                os.system("sudo reboot")
+                
         def list_model_entity(self):
                 model_files = os.getcwd() + "/Desktop/Main/models_dir/" + self.model + "/labelmap.txt"
                 list_model_files = os.getcwd() + "/Desktop/Main/models_dir/" + self.model
